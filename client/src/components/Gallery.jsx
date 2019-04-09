5f73daa16e2e651454a8846e28f9ab29f96dcd22
Michael Roeslein <michaeljroeslein@gmail.com>
2019-04-09 14:57:15 -0400

    Changed relative file path

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -26,7 +26,7 @@ class Gallery extends Component {
   componentDidMount() {
     const { path } = this.state;
     axios
-      .get(`/api/${path}`)
+      .get(`http://18.223.149.128:80/api/${path}`)
       .then(({ data }) => {
         return Object.entries(data[0])
           .filter(listing => listing[0] !== 'id')
